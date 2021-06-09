213f2b6aeb1e358645aba7687bd1290894d7e161
svetko mitrov <svetozarmitrov@gmail.com>
2021-06-09 21:38:41 +0300

    Added a model for the task.

---
+++ project8/project8/Entities/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project8.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Duration { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ResumedAt { get; set; }
+        public DateTime FinishedAt { get; set; }
+    }
+}
--- project8/project8/Models/IdentityModels.cs
+++ project8/project8/Models/IdentityModels.cs
@@ -24,18 +24,4 @@ namespace project8.Models
         public ApplicationRole(string roleName) : base(roleName) { }
 
     }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-
-    }
 }
